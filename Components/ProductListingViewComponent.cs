c1d63968eff8770c3d11695f09e169b059148f25
HassanTofayli <hassantofayli2@gmail.com>
2023-09-03 20:12:24 +0300

    View component, using invoke and method name and custome name using tag helpers

---
+++ Components/ProductListingViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using AspNet6Course.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNet6Course.Components
+{
+	[ViewComponent(Name = "VcCustom")]
+	public class ProductListingViewComponent : ViewComponent
+	{
+		private AppDbContext _context;
+		public ProductListingViewComponent(AppDbContext context)
+		{
+			_context = context;
+		}
+		public string Invoke()
+		{
+			return $"There are {_context.Products.Count()} Products";
+		}
+	}
+}
